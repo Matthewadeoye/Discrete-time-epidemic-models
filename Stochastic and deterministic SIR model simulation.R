2b3ba9d646479fd0620ff8691d779d1465fb684e
Matthew Adeoye <95089879+Matthewadeoye@users.noreply.github.com>
2023-05-23 17:02:39 +0100

    Updated

--- Stochastic and deterministic SIR model simulation.R
+++ Stochastic and deterministic SIR model simulation.R
@@ -1,103 +1,116 @@
-
-#First trial & correct
-Deterministic_DT_SIR_model1<- function(N,S0,I0,time,beta,gamma){
-  S<- numeric() #create empty vector to store simulations for susceptibles
-  I<- numeric() #create empty vector to store simulations for infectives
-  R<- numeric() #create empty vector to store simulations for removed
+#First trial
+Deterministic_DT_SIR_model1<- function(N,S0,I0,minTime,maxTime,beta,gamma,step_size,plot=TRUE){
+  minTime<- minTime           #start time
+  maxTime<- maxTime         #end time
+  step_size<- step_size      #step size
+  steps<- seq(minTime,maxTime, by=step_size)   #time discretization
+  beta<- beta           #infection rate
+  gamma<- gamma          #recovery rate
+  S<- numeric(length(steps)) #create empty vector to store simulations for susceptibles
+  I<- numeric(length(steps)) #create empty vector to store simulations for infectives
+  R<- numeric(length(steps)) #create empty vector to store simulations for removed
   S[1]<- S0  #initial number of susceptibles
   I[1]<- I0   #initial number of infectives
   R[1]<- N-S0-I0      #initial number of Removed
-  t<- time           #vector of time
-  b<- beta           #infection rate
-  g<- gamma          #recovery rate
   #loop and update compartments
-  for(tym in 2:length(t)){
-    S[tym]<- S[tym-1]-b*S[tym-1]*I[tym-1]
-    I[tym]<- I[tym-1]+b*S[tym-1]*I[tym-1]-g*I[tym-1]
-    R[tym]<- R[tym-1]+g*I[tym-1]
+  for(tym in 2:length(steps)){
+    S[tym]<- S[tym-1]-(beta*S[tym-1]*I[tym-1])*step_size
+    I[tym]<- I[tym-1]+(beta*S[tym-1]*I[tym-1]-gamma*I[tym-1])*step_size
+    R[tym]<- R[tym-1]+gamma*I[tym-1]*step_size
   }
-  #plot results
-  plot(t, S, type = "l", col = "blue", xlab = "Time", ylab = "Population", main = "Discrete-time deterministic SIR Model")
-  lines(t, I, col = "red")
-  lines(t, R, col = "green")
-  legend("topright", legend = c("Susceptible", "Infected", "Recovered"), col = c("blue", "red", "green"), lty = 1)
+  #results
+  simulations <- data.frame(Steps=steps, S = S, I = I, R = R)
+  if(plot){
+  graph<- plot(steps, S, type = "l", col = "blue", xlab = "Time", ylab = "Population", main = "Deterministic discrete-time SIR Model1",ylim=c(0,N))
+  lines(steps, I, col = "red")
+  lines(steps, R, col = "green")
+  legend("topright", legend = c("Susceptible", "Infected", "Removed"), col = c("blue", "red", "green"), lty = 1)
+  
+  return (list(simulations, graph))
+}else{
+  return(list(simulations=simulations))
 }
-Deterministic_DT_SIR_model1(N=11000,S0=10000,I0=1000,time=(1:400),beta=0.0001,gamma=0.05,step_size=1)
+}
+Deterministic_DT_SIR_model1(N=1000,S0=900,I0=100,minTime=1,maxTime=100,beta=0.001,gamma=0.05, step_size=0.03,plot=T)
+
 
 
 #Second trial assuming infections occur at the point of a Poisson process
-Deterministic_DT_SIR_model2<- function(N,S0,I0,time,beta,gamma,step_size){
-S<- numeric() #create empty vector to store simulations for susceptibles
-I<- numeric() #create empty vector to store simulations for infectives
-R<- numeric() #create empty vector to store simulations for removed
-b<- beta
-g<- gamma
-t <- time  # A vector of total time period
-h <- step_size   # Time discretization step size 
-total_steps<- length(time)  #Total number of simulations 
-
-# The initial susceptibe, infected and recovered at the start
-S[1] <- S0
-I[1] <- I0
-R[1] <- N-S0-I0
-
-#Loop through discretized time and Update the compartments at each step
-for (tym in 2:total_steps){
-#Assuming infections occur at the point of a Poisson process
-S[tym]<- S[tym-1] - S[tym-1]*(1-exp(-b*I[tym-1]*h))
-I[tym]<- I[tym-1] + S[tym-1]*(1-exp(-b*I[tym-1]*h))-I[tym-1]*(1-exp(-g*h))
-R[tym]<- R[tym-1] + I[tym-1]*(1-exp(-g*h))
+Deterministic_DT_SIR_model2<- function(N,S0,I0,minTime,maxTime,beta,gamma,step_size,plot=TRUE){
+  minTime<- minTime           #start time
+  maxTime<- maxTime           #end time
+  step_size<- step_size      #step size
+  steps<- seq(minTime,maxTime, by=step_size)   #time discretization
+  beta<- beta            #infection rate
+  gamma<- gamma          #recovery rate
+  S<- numeric(length(steps)) #create empty vector to store simulations for susceptibles
+  I<- numeric(length(steps)) #create empty vector to store simulations for infectives
+  R<- numeric(length(steps)) #create empty vector to store simulations for removed
+  S[1]<- S0  #initial number of susceptibles
+  I[1]<- I0   #initial number of infectives
+  R[1]<- N-S0-I0      #initial number of Removed
+  #Loop through discretized time and Update the compartments at each step
+  for (tym in 2:length(steps)){
+    #Assuming infections occur at the point of a Poisson process
+    S[tym]<- S[tym-1] - S[tym-1]*(1-exp(-beta*I[tym-1]*step_size))
+    I[tym]<- I[tym-1] + S[tym-1]*(1-exp(-beta*I[tym-1]*step_size))-I[tym-1]*(1-exp(-gamma*step_size))
+    R[tym]<- R[tym-1] + I[tym-1]*(1-exp(-gamma*step_size))
+  }
+  #results
+  simulations <- data.frame(Steps=steps,S = S, I = I, R = R)
+  if(plot){
+  graph<- plot(steps, S, type = "l", col = "blue", xlab = "Time", ylab = "Population", main = "Deterministic discrete-time SIR Model2",ylim=c(0,N))
+  lines(steps, I, col = "red")
+  lines(steps, R, col = "green")
+  legend("topright", legend = c("Susceptible", "Infected", "Recovered"), col = c("blue", "red", "green"), lty = 1)
+  return (list(simulations, graph))
+}else{
+  return(list(simulations=simulations))
 }
-#plot results
-plot(t, S, type = "l", col = "blue", xlab = "Time", ylab = "Population", main = "Discrete-time deterministic SIR Model")
-lines(t, I, col = "red")
-lines(t, R, col = "green")
-legend("topright", legend = c("Susceptible", "Infected", "Recovered"), col = c("blue", "red", "green"), lty = 1)
 }
-Second_DT_SIR_model(N=11000,S0=10000,I0=1000,time=(1:400),beta=0.0001,gamma=0.05,step_size=1)
-
+Deterministic_DT_SIR_model2(N=1000,S0=900,I0=100,minTime=1,maxTime=1000,beta=0.0001,gamma=0.05,step_size=0.03,plot=T)
 
 
 
 #Stochastic simulation
-Stochastic_DT_SIR_model<- function(N,S0,I0,time,beta,gamma,step_size){
-t <- time  # Total period in days
-h <- step_size  # Time discretization step size
-total_steps <- length(t)  # Total number of time steps
-
-# Initialize compartments
-S <- numeric()
-I <- numeric()
-R <- numeric()
-
-# Initial values
-b <- beta
-g <- gamma
-S[1] <- S0
-I[1] <- I0
-R[1] <- N-S0-I0
-
-# Simulation loop
-for (tym in 2:total_steps) {
-  # Assuming infections occur at the point of a Poisson process
-  p_I <- 1 - exp(-b * I[tym-1] * h)  # Probability of infection
-  p_R <- 1 - exp(-g * h)  # Probability of recovery
-  
-  # sample infections and recoveries from a binomial trial
-  delta_S <- rbinom(1, S[tym-1], p_I)
-  delta_I <- rbinom(1, I[tym-1], p_R)
-  
-  # Update the compartments
-  S[tym] <- S[tym-1] - delta_S
-  I[tym] <- I[tym-1] + delta_S - delta_I
-  R[tym] <- R[tym-1] + delta_I
+Stochastic_DT_SIR_model<- function(N,S0,I0,minTime,maxTime,beta,gamma,step_size,plot=TRUE){
+  minTime<- minTime           #start time
+  maxTime<- maxTime         #end time
+  step_size<- step_size      #step size
+  steps<- seq(minTime,maxTime, by=step_size)   #time discretization
+  beta<- beta           #infection rate
+  gamma<- gamma          #recovery rate
+  S<- numeric(length(steps)) #create empty vector to store simulations for susceptibles
+  I<- numeric(length(steps)) #create empty vector to store simulations for infectives
+  R<- numeric(length(steps)) #create empty vector to store simulations for removed
+  S[1]<- S0  #initial number of susceptibles
+  I[1]<- I0   #initial number of infectives
+  R[1]<- N-S0-I0      #initial number of Removed
+  #Loop through discretized time and Update the compartments at each step
+  # Simulation loop
+  for (tym in 2:length(steps)) {
+    # Assuming infections occur at the point of a Poisson process
+    p_I <- 1 - exp(-beta*I[tym-1]*step_size)  # Probability of infection
+    p_R <- 1 - exp(-gamma*step_size)  # Probability of recovery
+    
+    # sample infections and recoveries from a binomial trial
+    delta_S <- rbinom(1, S[tym-1], p_I)
+    delta_I <- rbinom(1, I[tym-1], p_R)
+    
+    # Update the compartments
+    S[tym] <- S[tym-1] - delta_S
+    I[tym] <- I[tym-1] + delta_S - delta_I
+    R[tym] <- R[tym-1] + delta_I
+  }
+  simulations <- data.frame(Steps=steps,S = S, I = I, R = R)
+  if(plot){
+  graph<- plot(steps, S, type = "l", col = "blue", xlab = "Time", ylab = "Population", main = "Stochastic discrete-time SIR Model",ylim=c(0,N))
+  lines(steps, I, col = "red")
+  lines(steps, R, col = "green")
+  legend("topright", legend = c("Susceptible", "Infected", "Recovered"), col = c("blue", "red", "green"), lty = 1)
+  return (list(simulations, graph))
+}else{
+  return(list(simulations=simulations))
 }
-
-plot(t, S, type = "l", col = "blue", xlab = "Time", ylab = "Population", main = "Stochastic SIR Model")
-lines(t, I, col = "red")
-lines(t, R, col = "green")
-legend("topright", legend = c("Susceptible", "Infected", "Recovered"), col = c("blue", "red", "green"), lty = 1)
 }
-Stochastic_DT_SIR_model(N=11000,S0=10000,I0=1000,time=(1:400),beta=0.0001,gamma=0.05,step_size=1)
-  
-
+Stochastic_DT_SIR_model(N=1000,S0=900,I0=100,minTime=1,maxTime=365,beta=0.0001,gamma=0.05,step_size=0.03,plot=T)
